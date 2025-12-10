8b5d13259f91b4e22ec36eede669d2830a87e9e1
Choong-Hoon Lee <choonghoon.lee@thakicloud.co.kr>
2025-12-10 11:13:24 +0900

    test again

--- vulnerable_app/app.py
+++ vulnerable_app/app.py
@@ -16,6 +16,7 @@ def vulnerable_logic(user_input):
     cursor = conn.cursor()
     query = f"SELECT * FROM users WHERE username = '{user_input}'" 
     cursor.execute(query) 
+    a = 100
 
     # [CodeQL Test 2: Command Injection]
     # 사용자 입력을 쉘 명령어에 그대로 전달 (shell=True)
