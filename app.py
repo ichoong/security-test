c3e518bac06bba7e8a5403d199b3ab7b1db33dd9
Choong-Hoon Lee <choonghoon.lee@thakicloud.co.kr>
2025-12-10 13:41:41 +0900

    gitleaks arg change

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def vulnerable_logic(user_input):
     cursor.execute(query) 
     a = 100
     b = 200
+    c = 100
 
     # [CodeQL Test 2: Command Injection]
     # 사용자 입력을 쉘 명령어에 그대로 전달 (shell=True)
